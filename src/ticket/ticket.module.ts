ce93750c2bf1c1b408a32ed6655d9a4b6d41e48d
DuKyHan <dukyhan2312@gmail.com>
2024-03-10 21:39:46 +0700

    updating

--- src/ticket/ticket.module.ts
+++ src/ticket/ticket.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from 'src/common/common.module';
 import { EmailModule } from 'src/email/email.module';
 import { TicketController } from './controllers';
 import { TicketService } from './services';
+import { NotificationModule } from 'src/notification/notification.module';
+
 
 @Module({
-  imports: [CommonModule, EmailModule],
+  imports: [CommonModule, EmailModule,NotificationModule],
   controllers: [TicketController],
   providers: [TicketService],
 })
